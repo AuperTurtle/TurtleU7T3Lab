55a450667b6e25bfb83464b589717a07c83ba032
1E10 Laptop <sample@bths.edu>
2023-02-02 10:58:59 -0500

    .

--- src/Digits.java
+++ src/Digits.java
@@ -1,2 +1,48 @@
-package PACKAGE_NAME;public class Digits {
+import java.util.ArrayList;
+
+public class Digits {
+    /**
+     * The list of digits from the number used to construct this object.
+     * The digits appear in the list in the same order in which they appear in the original number.
+     */
+    private ArrayList<Integer> digitList;
+
+    /**
+     * Constructs a Digits object that represents num.
+     * PRECONDITION: num >= 0
+     */
+    public Digits(int num) {
+        digitList = new ArrayList<Integer>();
+        if (num == 0) {
+            digitList.add(0);
+        }
+            while (num > 0) {
+                digitList.add(num % 10);
+                num = num / 10;
+            }
+    }
+
+
+
+    // GETTER METHOD: added to enable test code (not provided in original FRQ)
+    public ArrayList<Integer> getDigitList() {
+        return digitList;
+    }
+
+    /**
+     * Returns true if the digits in this Digits object are in strictly increasing order;
+     * false otherwise.
+     */
+    public boolean isStrictlyIncreasing() {
+        if (digitList.size() == 1) {
+            return true;
+        }
+        for (int i = 0; i < digitList.size() - 1; i++) {
+            if (!(digitList.get(i) > digitList.get(i + 1))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // create ArrayList from a list of initial values; requires Arrays.asList
+        ArrayList<Integer> numList = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 5, 6, 7, 8, 8, 9, 10, 11));
+
+        for (int i = 1; i < numList.size() - 2; i++) {
+            if (numList.get(i - 1) % 2 == 0) {
+                numList.set(i, numList.get(i + 1) + numList.get(i + 2));
+            }
+        }
+        System.out.println(numList);
+
+        int num = 157;
+        while (num > 0) {
+            System.out.println(num % 10);
+            num = num / 10;
+        }
+
+
     }
 }
